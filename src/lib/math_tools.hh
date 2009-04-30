e1d0cba489ce8b46c5ba0133720e8b3424bc4fba
glenux <glenux@eaee96b3-f302-0410-b096-c6cfd47f7835>
2009-04-30 14:12:47 +0000

    cryptafinity lib : remove extraneous qualification MathTools:: .
    git-svn-id: https://websvn.glenux.net/svn/Upoc/cryptaffinity/trunk@1335 eaee96b3-f302-0410-b096-c6cfd47f7835

--- src/lib/math_tools.hh
+++ src/lib/math_tools.hh
@@ -5,17 +5,18 @@
 #include "cryptafinity_iface.hh"
 
 namespace LibCryptAfinity {
+
 	class MathTools {
 		private:
-			std::vector<int> MathTools::pgcd_real(int * reste, int * quotient, int * coef_first, int * coef_second, int pas);
+			std::vector<int> pgcd_real(int * reste, int * quotient, int * coef_first, int * coef_second, int pas);
 
 		public:
 			int pgcd(int a, int b);
 			int ppcm(int a, int b);
 
-			std::vector<int> MathTools::pgcd_mod(int a, int b);
+			std::vector<int> pgcd_mod(int a, int b);
 			
-	int MathTools::inverse(int a, int modulo);
+			int inverse(int a, int modulo);
 
 	};
 }
